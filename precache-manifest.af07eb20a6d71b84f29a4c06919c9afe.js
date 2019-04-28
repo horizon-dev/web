db4bdd5e868667a20b55a1f514c3e51eb2307e1d
danacr <dan@kryha.io>
2019-04-28 13:43:10 +0300

    first commit

---
+++ precache-manifest.af07eb20a6d71b84f29a4c06919c9afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f43e4c5c647f8f4bc0e2",
+    "url": "/static/css/main.22fb7162.chunk.css"
+  },
+  {
+    "revision": "f43e4c5c647f8f4bc0e2",
+    "url": "/static/js/main.3d8e4276.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "34fff7ab7d03037601d5",
+    "url": "/static/css/2.d98134be.chunk.css"
+  },
+  {
+    "revision": "34fff7ab7d03037601d5",
+    "url": "/static/js/2.dd441131.chunk.js"
+  },
+  {
+    "revision": "3af293896ce37a00114a6b5f688ce54e",
+    "url": "/index.html"
+  }
+];
