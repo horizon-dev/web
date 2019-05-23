cee71a3c3508858f977b5d6c588a87854c373141
danacr <dan@kryha.io>
2019-05-23 14:08:27 +0200

    added demo

---
+++ demo/precache-manifest.607eefc3ae7e750d0b584755386bfd97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39ea58cc2b4ce89cde036f9faf1d538",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af8e211c9faab5517cd",
+    "url": "/static/css/main.c8804015.chunk.css"
+  },
+  {
+    "revision": "657cd6c04ef1f29f2cc8",
+    "url": "/static/js/2.7b492d4d.chunk.js"
+  },
+  {
+    "revision": "5af8e211c9faab5517cd",
+    "url": "/static/js/main.0178a429.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
